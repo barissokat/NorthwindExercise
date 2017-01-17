4c5024ef9ec8e01aefa223fc761518db73ed6a9a
bsokat <barissokat@gmail.com>
2017-01-17 19:07:57 +0300

    Created Category Controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using NorthwindExercise.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwindExercise.Controllers
+{
+    public class CategoryController : Controller
+    {
+        NorthwindContext db = new NorthwindContext();
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+    }
+}
