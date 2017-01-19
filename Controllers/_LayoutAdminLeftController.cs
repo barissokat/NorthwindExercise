0ebd21a488ac6612013c6d977bc23f03bae54a4a
bsokat <barissokat@gmail.com>
2017-01-19 11:06:50 +0300

    Added controller for layout

---
+++ Controllers/_LayoutAdminLeftController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwindExercise.Controllers
+{
+    public class _LayoutAdminLeftController : Controller
+    {
+        // GET: _LayoutAdminLeft
+        public ActionResult _LayoutAdminLeft()
+        {
+            return View();
+        }
+    }
+}
