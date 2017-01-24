abd67903bb444f22812acf1263674402934ac734
bsokat <barissokat@gmail.com>
2017-01-24 14:23:46 +0300

    Added Category Controllers

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -6,22 +6,21 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using NorthwindExercise.DAL;
 using NorthwindExercise.Models;
 
 namespace NorthwindExercise.Controllers
 {
-    public class CategoryController : Controller
+    public class CategoriesController : Controller
     {
-        private NorthwindContext db = new NorthwindContext();
+        private NORTHWNDEntities db = new NORTHWNDEntities();
 
-        // GET: Category
+        // GET: Categories
         public ActionResult Index()
         {
             return View(db.Categories.ToList());
         }
 
-        // GET: Category/Details/5
+        // GET: Categories/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -36,13 +35,13 @@ namespace NorthwindExercise.Controllers
             return View(category);
         }
 
-        // GET: Category/Create
+        // GET: Categories/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Category/Create
+        // POST: Categories/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -59,7 +58,7 @@ namespace NorthwindExercise.Controllers
             return View(category);
         }
 
-        // GET: Category/Edit/5
+        // GET: Categories/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -74,7 +73,7 @@ namespace NorthwindExercise.Controllers
             return View(category);
         }
 
-        // POST: Category/Edit/5
+        // POST: Categories/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -90,7 +89,7 @@ namespace NorthwindExercise.Controllers
             return View(category);
         }
 
-        // GET: Category/Delete/5
+        // GET: Categories/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -105,7 +104,7 @@ namespace NorthwindExercise.Controllers
             return View(category);
         }
 
-        // POST: Category/Delete/5
+        // POST: Categories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
